6ae1a16f0fb18de6649fb8e98163f7cd9941e2dc
Zp <zhaopanhw@foxmail.com>
2019-04-10 00:46:29 +0800

    完成antd配置

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Button } from 'antd';
+import * as React from 'react';
+import '@/App.css';
 
-class App extends Component {
-  render() {
+import logo from './logo.svg';
+
+class App extends React.Component {
+  public render() {
     return (
       <div className="App">
-      <div>react</div>
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.tsx</code> and save to reload.
+          <Button type="primary">Button</Button>
+        </p>
       </div>
     );
   }
