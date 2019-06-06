667b89dda409df9daf68f05b1d68bf043b8281d8
Administrator <zhaopanhw@foxmail.com>
2019-06-06 17:45:25 +0800

    decorator

--- src/view/functionalTest/tsdecorator/index.tsx
+++ src/view/functionalTest/tsdecorator/index.tsx
@@ -17,13 +17,14 @@ class TsDecorator extends React.Component<TsDecoratorProps, TsDecoratorState> {
     }
     componentDidMount() {
 
+        // 类的装饰器 重载构造函数
+        this.overloadConstructor();
         //类方法的装饰器
         this.funDecorator();
-        //类的装饰器
-        this.classDecorator();
-        // 重载构造函数
-        this.overloadConstructor();
-
+        //访问器装饰器
+        this.accessorDecorator();
+        // 属性修饰器
+        this.propertyDecorator();
     }
     public funDecorator = () => {
         let temple;
@@ -45,55 +46,38 @@ class TsDecorator extends React.Component<TsDecoratorProps, TsDecoratorState> {
 
         console.log(P.prototype === temple);
     }
-    /**
-     * classDecorator
-     */
-    public classDecorator() {
-        interface FooInterface {
-            new(): P
-        }
-        interface PInterface {
-            new(): Function
-        }
-        let temple
-        //当装饰函数直接修饰类的时候，装饰函数接受唯一的参数，这个参数就是该被修饰类本身。上述的例子中，输出的target就是类P的本身。
-        function foo(target: FooInterface) {
-            return class extends target {
-                name = 'Jony';
-                sayHello() {
-                    console.log("Hello " + this.name)
-                }
-            }
-        }
-        @foo
-        class P {
-            constructor() {
-
-            }
-        }
-
-        const p = new P();
-        console.log(p)
-        console.log(new P())
-        console.log(P.prototype)
-        // console.log(temple === P) //true
-    }
 
     /**
      * 重载构造函数
      */
     public overloadConstructor() {
-        function classDecorator<T extends { new(...args: any[]): {} }>(constructor: T) {
+        // 类装饰器  重载构造函数
+        function classDecorator<T extends { new(...args: any[]): object }>(constructor: T): T {
             return class extends constructor {
                 newProperty = "new property";
                 hello = "override";
-                public newPropertyFun=()=>{
+                // this.newPropertyFun().bind(this);
+                newPropertyFun = () => {//使用箭头函数自动绑定当前类this对象
                     console.log("this is new  " + this.newProperty)
+                    // return "this is new  " + this.newProperty
                 }
             }
         }
 
+        // 采用工厂方式创建装饰器
+        function testFactory(n: boolean) {
+            return function (target: string, key: string, desc: PropertyDescriptor) {
+                console.log("\n -->", target, "\nkey -->", key, "\ndesc -->", desc);
+            }
+        }
+        function Path(path: string) {
+            return function (target: Function) {
+                !target.prototype.$Meta && (target.prototype.$Meta = {})
+                target.prototype.$Meta.baseUrl = path;
+            };
+        }
         @classDecorator
+        // @Path('/hello')
         class Greeter {
             property = "property";
             hello: string;
@@ -102,10 +86,68 @@ class TsDecorator extends React.Component<TsDecoratorProps, TsDecoratorState> {
             }
         }
 
-        const greeter = new Greeter();
-        console.log(greeter);
-        console.log(Greeter.prototype);
-        console.log(new Greeter("world"));
+        const greeter: any = new Greeter();
+        // console.log(greeter);
+        // greeter.newPropertyFun()
+    }
+
+    /**
+     * 访问器修饰器
+     */
+    public accessorDecorator() {
+        function configurable(value: boolean) {
+            return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+                descriptor.configurable = value;
+                descriptor.enumerable = value;
+            };
+        }
+        class Point {
+            private _x: number;
+            private _y: number;
+            private _z: number;
+            constructor(x: number, y: number,z: number) {
+                this._x = x;
+                this._y = y;
+                this._z = z;
+            }
+
+            // @configurable(true)
+            get a() { return this._z; }
+            get z() { return this._z; }
+
+            @configurable(true)
+            get y() { return this._y; }
+
+            /**
+             * changey
+             */
+            public changey() {
+                this._y=12;
+                console.log(this.y);
+                return this._y
+            }
+        }
+        const point:any =new Point(1,2,3);
+        console.log(point.y);
+        console.log(point.changey());
+    }
+
+    /**
+     * propertyDecorator
+     */
+    public propertyDecorator() {
+        // class Greeter {
+        //     @format("Hello, %s")
+        //     greeting: string;
+        
+        //     constructor(message: string) {
+        //         this.greeting = message;
+        //     }
+        //     greet() {
+        //         let formatString = getFormat(this, "greeting");
+        //         return formatString.replace("%s", this.greeting);
+        //     }
+        // }
     }
     render() {
         return (<div>{this.state.name}</div>);
